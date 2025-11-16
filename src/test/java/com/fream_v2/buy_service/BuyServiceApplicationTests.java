bd5cad56d845fc0e20574bce080bacf223818666
jun <pickjog@gmail.com>
2025-11-16 21:14:37 +0900

    first commit

---
+++ src/test/java/com/fream_v2/buy_service/BuyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fream_v2.buy_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
